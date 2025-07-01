704e8412bc292919e9f6cb700d0e07b8bc6ede2c
Nhật Anh <pna120104@gmail.com>
2025-07-01 20:51:14 +0700

    fix

--- src/main/java/vn/aptech/java/controllers/user/UserController.java
+++ src/main/java/vn/aptech/java/controllers/user/UserController.java
@@ -2,11 +2,54 @@ package vn.aptech.java.controllers.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/user/homepage")
-    public String index() {
+
+    @GetMapping("/homepage")
+    public String homepage() {
+        return "user/pages/homepage";
+    }
+
+    @GetMapping("/check_warranty")
+    public String checkWarranty() {
         return "user/pages/check_warranty";
+    }
+
+    @GetMapping("/schedule_warranty")
+    public String scheduleWarranty() {
+        return "user/pages/schedule_warranty";
+    }
+
+    @GetMapping("/history")
+    public String history() {
+        return "user/pages/history";
+    }
+
+    @GetMapping("/search_parts")
+    public String searchParts() {
+        return "user/pages/search_parts";
+    }
+
+    @GetMapping("/support")
+    public String support() {
+        return "user/pages/support";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "user/pages/contact";
+    }
+
+    @GetMapping("/information")
+    public String information() {
+        return "user/pages/information";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/pages/login";
     }
 }
